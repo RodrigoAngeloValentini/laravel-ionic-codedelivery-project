309bd45f4f7b586a369b71e4e662053eb817d2bd
Rodrigo Angelo Valentini <rodrigoangelovalentini@hotmail.com>
2016-04-27 13:08:43 -0300

    oauth2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -56,3 +56,17 @@ Route::group(['prefix' => 'customer', 'as' => 'customer.','middleware' => 'auth.
     Route::post('order/store', ['as' => 'order.store', 'uses' => 'CheckoutController@store']);
 });
 
+Route::post('oauth/access_token', function() {
+    return Response::json(Authorizer::issueAccessToken());
+});
+
+Route::group(['prefix' => 'api', 'middleware' => 'oauth' , 'as' => 'api.'], function()
+{
+    Route::get('pedidos',function(){
+        return [
+            'id' => 1,
+            'client' => 'Rodrigo',
+            'total' => 10
+        ];
+    });
+});
